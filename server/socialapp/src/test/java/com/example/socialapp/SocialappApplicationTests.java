da3fd89ec74d821b1b31cad8c0cf145192b41bd8
Mahi <abeysinghemahi@gmail.com>
2025-03-15 19:30:51 +0530

    add server

---
+++ server/socialapp/src/test/java/com/example/socialapp/SocialappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socialapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
